1c1c5686e1590468e75a987e24f8cc4693b54b16
Tom Wilson <tom.wilson@liquidint.com>
2020-03-25 10:39:49 -0400

    Updates

---
+++ precache-manifest.20ca807e6386b121cf93f2c88797de9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b8f6cfc039df24c48f89b346d27b27",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "7e88ec7a1643e8fa977a",
+    "url": "/monster-rolodex/static/css/main.e9965316.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monster-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e88ec7a1643e8fa977a",
+    "url": "/monster-rolodex/static/js/main.a004b041.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
